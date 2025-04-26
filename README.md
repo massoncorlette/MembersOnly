20cd391aac58bbf3516319385e60113a85c593d6
massoncorlette <massoncorlette07@gmail.com>
2025-04-26 00:33:59 -0400

    setting up script file for db, readme, and file directorys

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # AuthenticationPractice
+
+Run:
+npm install express express-session pg passport passport-local ejs 
+-- installs all in one go for express session password encryption
+
+npm install express-validator -- for sanitation and validation
+npm install dotenv -- for storing secure user data in env file   (put require('dotenv').config() in root file to enable access in .env file); 
+
+npm install bcryptjs -- for serializing passwords
+npm install connect-pg-simple -- session storage (connecting db to session storage)
+
 Reference commits for implementation details for passport middleware authentication
 Introduction to user authentication using passport.js middleware
---
+++ db/setupdb.js
@@ -0,0 +1,44 @@
+const { Client } = require("pg");
+
+require("dotenv").config();
+
+const { argv } = require("node:process");
+
+// script here to insert dummy data upon deployment
+
+const DefaultSQL = `
+
+CREATE TABLE IF NOT EXISTS users (
+  user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first VARCHAR ( 20 ),
+  last VARCHAR ( 20 ),
+  email VARCHAR ( 255 ),
+);
+
+
+
+CREATE TABLE IF NOT EXISTS membermessages (
+  message_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  message VARCHAR ( 2000 ),
+  user_id INTEGER,
+  FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+
+`;
+
+
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: argv[2] || process.env.DATABASE_URL,
+  });
+
+  await client.connect();
+  await client.query(DefaultSQL);
+  await client.end();
+  console.log("done");
+};
+
+main();
